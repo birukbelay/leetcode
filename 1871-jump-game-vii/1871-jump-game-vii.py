57d24c9a0db1ac45b32a7f40c172ef81342c6ba9
Biruk Belay <56615724+birukbelay@users.noreply.github.com>
2022-11-12 06:46:54 +0300

    Time: 1495 ms (26.85%), Space: 18 MB (65.16%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.py
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -1,15 +1,31 @@
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
         # dp[i] = true if we can reach s[i]
-        dp = [v == '0' for v in s]
+#         dp = [v == '0' for v in s]
         
-        # pre means the number of previous position that we can jump from.
-        pre = 0
+#         # pre means the number of previous position that we can jump from.
+#         pre = 0
         
-        for i in range(1, len(s)):
-            if i >= minJump:
-                pre+= dp[i - minJump]
-            if i > maxJump:
-                pre -= dp[i- maxJump -1]
-            dp[i] = pre > 0 and s[i] == '0'
-        return dp[-1]
+#         for i in range(1, len(s)):
+#             if i >= minJump:
+#                 pre+= dp[i - minJump]
+#             if i > maxJump:
+#                 pre -= dp[i- maxJump -1]
+#             dp[i] = pre > 0 and s[i] == '0'
+#         return dp[-1]
+    
+        # bfs -------------------------------------------
+        q, farthest = deque([0]), 0
+        
+        while q:
+            i=q.popleft()
+            start = max(i + minJump, farthest +1)
+            end = min(i + maxJump +1, len(s)) # so it is not out fo bound
+            for j in range(start, end):
+                if s[j]=='0':
+                    q.append(j)
+                    if j == len(s)-1:
+                        return True
+            farthest = i+ maxJump
+        return False
+        
