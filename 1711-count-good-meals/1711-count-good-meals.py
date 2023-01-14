b4ba7c629f507ba4ac035a91719da7547b01b174
Biruk Belay <56615724+birukbelay@users.noreply.github.com>
2023-01-14 21:49:51 +0300

    Time: 1755 ms (67.89%), Space: 26.2 MB (24.95%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -0,0 +1,49 @@
+import collections
+class Solution:
+    def countPairs(self, deliciousness: List[int]) -> int:
+        #make an array that 
+        arr=[0]*22
+        for i in range(22):
+            arr[i] = 2**i
+        #count how many foods there are
+        d = collections.Counter(deliciousness)
+        tot=0
+        #
+        s=set(deliciousness)
+        print(s)
+        print(d)
+        #set of deliciousness
+        for i in deliciousness:
+            d[i]-= 1
+            for powr in arr:                
+                k= d.get(powr-i, 0)
+                tot+=k               
+                
+                    
+        # for i in s:
+        #     for powr in arr:
+        #         if powr-i in d:
+        #             if i==powr-i:
+        #                 if d[i]>2:                            
+        #                     tot+= d[i]                            
+        #             else:
+        #                 tot+= d[i] * d[powr-i]
+        #     d.pop(i, None)
+        return tot%(10**9 + 7)
+        # [0,1]    
+                    
+                    
+                    
+                    
+            
+            
+            
+            
+            
+
+        
+        
+        
+        
+        
+        
