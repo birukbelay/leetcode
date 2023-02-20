4c123a2b2b19cbad6913195a6025049f1fb22b84
Biruk Belay <56615724+birukbelay@users.noreply.github.com>
2023-02-21 02:26:33 +0300

    Time: 623 ms (24.01%), Space: 16.4 MB (50.00%) - LeetHub

---
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.py
@@ -0,0 +1,50 @@
+from collections import Counter
+class Solution:
+    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+        d= Counter(s)
+        arr=[]
+        
+        maxCtr=0
+        for i in range(len(dictionary)):
+            j=dictionary[i]
+            
+            # cheking that s have all letters in j in order
+            l1=l2=0
+            while l2< len(j) and l1<len(s):
+                if j[l2] ==s[l1]:
+                    l2+=1
+                    l1+=1
+                else:
+                    l1+=1
+            if l2!=len(j):
+                continue
+            # counting the length of j 
+            temp = Counter(list(j))
+            ctr=0
+            brk=True
+            for k,v in temp.items():
+                
+                if k in d and v<= d[k]:
+                    ctr+=v
+                else:
+                    brk=False
+                    break
+            if brk:
+                if ctr>maxCtr:
+                    maxCtr=ctr
+                    arr=[j]
+                elif ctr==maxCtr:
+                    arr.append(j)
+        
+        if arr:
+            arr.sort()
+            # print(maxCtr)
+            return arr[0]
+        else:
+            return ""
+            
+                
+                    
+
+        
+        
