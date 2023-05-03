54ae2e907deb067580250943a0ca61954c005229
Biruk Belay <56615724+birukbelay@users.noreply.github.com>
2023-05-03 22:45:00 +0300

    Time: 5115 ms (30.88%), Space: 70 MB (32.50%) - LeetHub

---
+++ 204-count-primes/204-count-primes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n<3:
+            return 0
+        is_prime: list[bool] = [True for _ in range(n)]
+        is_prime[0] = is_prime[1] = False
+        i = 2
+
+        while i * i < n:
+            if is_prime[i]:
+                j = i * i
+                while j < n:
+                    is_prime[j] = False
+                    j += i
+            i += 1
+        ctr=0
+        for i in is_prime:
+            if i:
+                ctr+=1
+        return ctr
