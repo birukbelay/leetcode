8bd04ac76f51beb56a5a9462162182d3dd1e6bc0
Biruk Belay <56615724+birukbelay@users.noreply.github.com>
2023-05-07 11:25:35 +0300

    Time: 5155 ms (31.35%), Space: 70 MB (34.06%) - LeetHub

--- 204-count-primes/204-count-primes.py
+++ 204-count-primes/204-count-primes.py
@@ -4,11 +4,14 @@ class Solution:
             return 0
         is_prime: list[bool] = [True for _ in range(n)]
         is_prime[0] = is_prime[1] = False
+        
         i = 2
-
+        # we check untill square root bcs others are checked already by bottom numbers 
         while i * i < n:
             if is_prime[i]:
-                j = i * i
+                # because bottom nums are already checked
+                j = i * i # was j= 2 * i ()
+                # filtering gout the non prime numbers
                 while j < n:
                     is_prime[j] = False
                     j += i
