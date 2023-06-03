f41b3b8094601d0acf4e90ec115bb4f75ee574ae
Biruk Belay <56615724+birukbelay@users.noreply.github.com>
2023-06-04 00:21:37 +0300

    Time: 134 ms (93.45%), Space: 64.2 MB (68.62%) - LeetHub

---
+++ 2631-group-by/2631-group-by.ts
@@ -0,0 +1,22 @@
+declare global {
+    interface Array<T> {
+        groupBy(fn: (item: T) => string): Record<string, T[]>
+    }
+}
+
+Array.prototype.groupBy = function(fn) {
+    const returnObject = {};
+  for (const item of this) {
+    const key = fn(item);
+    if (key in returnObject) {
+      returnObject[key].push(item);
+    } else {
+      returnObject[key] = [item];
+    }
+  }
+  return returnObject;
+}
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
